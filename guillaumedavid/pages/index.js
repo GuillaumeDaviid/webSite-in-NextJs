95e4cc19fa5dda650dd5a2c1d347bf1186db4301
Guillaume David <guillaumedaavidpro@gmail.com>
2022-06-07 11:16:26 +0200

    Portfolio component

--- guillaumedavid/pages/index.js
+++ guillaumedavid/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import About from '../components/About'
+import Portfolio from '../components/Portfolio'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
 
       <Header />
       <About />
+      <Portfolio />
 
 
 
