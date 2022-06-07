bcefa17be86bf207e53c664cd43a28c943a4790b
Guillaume David <guillaumedaavidpro@gmail.com>
2022-06-07 11:02:05 +0200

    Header component

---
+++ guillaumedavid/components/Header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+    return(
+        <div className="Header">
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+            <h1>Guillaume David</h1>
+            <div className="Header_tiret"></div>
+            <h3>Développeur Front End / Javascript / React</h3>
+        </div>
+    )
+}
