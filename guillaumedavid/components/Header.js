629e9e60e5563d0807793f326a4fdf49b9074c30
Guillaume David <guillaumedaavidpro@gmail.com>
2022-06-08 15:57:27 +0200

    add fonts + style title and description header and background image

--- guillaumedavid/components/Header.js
+++ guillaumedavid/components/Header.js
@@ -1,16 +1,18 @@
 export default function Header() {
     return(
+        <section id="home">
         <div className="Header">
             <nav>
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                <li><a href="#home"><span>Accueil</span></a></li>
+                <li><a href="#portfolio"><span>Portfolio</span></a></li>
+                <li><a href="#contact"><span>Contact</span></a></li>
                 </ul>
             </nav>
-            <h1>Guillaume David</h1>
-            <div className="Header_tiret"></div>
-            <h3>Développeur Front End / Javascript / React</h3>
+            <h1 className="Header_title">GUILLAUME DAVID</h1>
+            <div className="Header_separator"></div>
+            <h3 className="Header_description">Développeur Front End / Javascript / React</h3>
         </div>
+        </section>
     )
 }
