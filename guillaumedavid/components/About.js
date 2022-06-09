c0d416edc0c3adafbb0439ac69607e6d96752e8e
Guillaume David <guillaumedaavidpro@gmail.com>
2022-06-09 12:20:37 +0200

    creativity section and style

--- guillaumedavid/components/About.js
+++ guillaumedavid/components/About.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function About() {
     return(
         <section id="introduction">
@@ -8,8 +10,13 @@ export default function About() {
                 Spécialisé <strong>Javascript / React.js</strong> et dans l&apos;intégration /
                 développement responsive <strong>HTML/CSS/JS</strong>. Je vous accompagne dans le développement de votre projet web. Vous retrouverez ici mon portfolio et un moyen de me contacter.</p>
 
-                <div>
-                <img />
+                <div className="creativity">
+                    <div className='creativity_block'>
+                <Link href={`https://codepen.io/GuillaumeDAv`}>
+                    
+                    <a className='creativity_btn' target="_blank">Créativité</a>
+                </Link>
+                </div>
                 </div>
                 
         </div>
--- guillaumedavid/next.config.js
+++ guillaumedavid/next.config.js
@@ -4,3 +4,11 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+/*
+module.exports = {
+  images: {
+    formats: ['image/png','image/jpeg'],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    eviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+}*/
