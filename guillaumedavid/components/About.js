99915b968a1d893ded84ebbd05d0351b6de1bca1
Guillaume David <guillaumedaavidpro@gmail.com>
2022-06-09 11:29:21 +0200

    style about component

--- guillaumedavid/components/About.js
+++ guillaumedavid/components/About.js
@@ -1,14 +1,18 @@
 export default function About() {
     return(
+        <section id="introduction">
         <div className="About">
-            <h2>A PROPOS</h2>
-            <p>Développeur web Front-End Freelance dans les Hauts-de-France (Lille), 
-                Spécialisé Javascript / React.js et dans l`&apos;`intégration / 
-                développement responsive HTML/CSS/JS. Je vous accompagne dans le développement de votre projet web. Vous retrouverez ici mon portfolio et un moyen de me contacter.</p>
+            <h2 className="About_title">A PROPOS</h2>
+            <div className="About_separator"></div>
+            <p className="About_text">Développeur web <strong>Front-End</strong> Freelance dans les Hauts-de-France (Lille), 
+                Spécialisé <strong>Javascript / React.js</strong> et dans l&apos;intégration /
+                développement responsive <strong>HTML/CSS/JS</strong>. Je vous accompagne dans le développement de votre projet web. Vous retrouverez ici mon portfolio et un moyen de me contacter.</p>
 
                 <div>
                 <img />
                 </div>
+                
         </div>
+        </section>
     )
 }
--- guillaumedavid/pages/_app.js
+++ guillaumedavid/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/Header.scss'
 import '../styles/stylesheets.scss'
+import '../styles/About.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
