5fcc4f9f74cd9026ee9d7f16da3a3f09212f8c6f
Guillaume David <guillaumedaavidpro@gmail.com>
2022-06-10 16:09:19 +0200

    style Article component + config to display image

--- guillaumedavid/components/Article.js
+++ guillaumedavid/components/Article.js
@@ -1,9 +1,33 @@
-export default function Article() {
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Article({posts}) {
+
+  const loaderProp =({ src }) => {
+    return src;
+}
+
+  console.log(posts)
     return(
-        <div>
+        <div className="Article">
             <h2>Article</h2>
-            <div></div>
-            <div></div>
+            <div className="Article_separator"></div>
+            <div className="Article_items">
+                <button className="Article_items-btn right"></button>
+                <button className="Article_items-btn left"></button>
+                <div className='Article_items-elt'>
+                {posts.items.map((data, index) => 
+                  <div key={index} className="Artcile_items-elt-item">
+                    <Image className='item_img' src={data.thumbnail} alt={data.categories[1]} width={"200px"} height={"200px"} loader={loaderProp}/>
+
+                    <Link href={data.link}>
+                    <a className='item_link' target="_blank"><h3>{data.title}</h3></a>
+                    </Link>
+                    
+                  </div> 
+                )}
+                </div>
+            </div>
         </div>
     )
 }
--- guillaumedavid/pages/_app.js
+++ guillaumedavid/pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/Header.scss'
 import '../styles/stylesheets.scss'
 import '../styles/About.scss'
 import '../styles/Portfolio.scss'
+import '../styles/Article.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
