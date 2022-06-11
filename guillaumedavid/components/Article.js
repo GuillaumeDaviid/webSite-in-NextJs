8d7c4b3d4cfc100b8ec910a0720ef00d4be09490
Guillaume David <guillaumedaavidpro@gmail.com>
2022-06-11 15:16:36 +0200

    handle click function

--- guillaumedavid/components/Article.js
+++ guillaumedavid/components/Article.js
@@ -1,7 +1,37 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import {useState} from 'react'
 
 export default function Article({posts}) {
+  const [p, setp] = useState(0);
+  const [nbr, setNbr] = useState(1);
+
+  function handleClickNext() {
+    let carrousel = document.getElementById('carrousel');
+    if (carrousel){
+        if (nbr < 7){
+            setNbr(nbr + 1);
+            const newp = p - 1
+            setp(newp);
+            carrousel.style.transform = "translate("+newp*25+"vw)";
+            carrousel.style.transition = "all 0.5s ease";
+        }
+    }
+}
+
+function handleClickPrevious() {
+    let carrousel = document.getElementById('carrousel');
+    if (carrousel){
+        if (nbr > 1){
+            setNbr(nbr - 1);
+            const newp = p + 1;
+            setp(newp);
+        carrousel.style.transform = "translate("+newp*25+"vw)";
+        carrousel.style.transition = "all 0.5s ease";
+        }
+        
+    }
+}
 
   const loaderProp =({ src }) => {
     return src;
@@ -11,10 +41,10 @@ export default function Article({posts}) {
             <h2>Article</h2>
             <div className="Article_separator"></div>
             <div className="Article_items">
-                <button className="Article_items-btn right"></button>
-                <button className="Article_items-btn left"></button>
+                <button className="Article_items-btn right" onClick={handleClickPrevious}></button>
+                <button className="Article_items-btn left" onClick={handleClickNext}></button>
 
-                <div className='Article_items-elt'>
+                <div className='Article_items-elt' id="carrousel">
 
                 {posts.items.map((data, index) => 
                   <div key={index} className="Article_items-elt-item">
