5fa3d58b94a3f2607bb72fe139e8f30180cc58e7
Guillaume David <guillaumedaavidpro@gmail.com>
2022-06-13 15:31:24 +0200

    Form component: style form

--- guillaumedavid/components/Form.js
+++ guillaumedavid/components/Form.js
@@ -9,7 +9,8 @@ export default function Form() {
             Vous cherchez un développeur qui répond à vos besoins sur-mesure et qui respect les délais et le budget ? Demandez un Devis !</p>
             </div>
 
-            <div>
+            <div className="Form_content">
+            <div className="Form_content-info">
                 <h4>COORDONNEES</h4>
                 <p>Loos-en-Gohelle (62) - France</p>
                 <p>guillaumedaavidpro@gmail.com</p>
@@ -20,24 +21,30 @@ export default function Form() {
                 <a>linkedin</a>
             </div>
 
-            <div>
+            <div className="Form_content-form">
                 <form>
-                    <div>
-                        <input type="text" name="user_name" placeholder="Nom"/>
+                    <div className="row_form">
+
+                    <div >
+                        <input className="row_form-name" type="text" name="user_name" placeholder="Nom"/>
+                    </div>
+
+                    <div >
+                        <input className="row_form-mail" type="email" name="user_mail" placeholder="Email"/>
+                    </div>
+
                     </div>
 
                     <div>
-                        <input type="email" name="user_mail" placeholder="Email"/>
+                        <textarea className="row_form-msg" name="user_message" placeholder="Message"/>
                     </div>
 
-                    <div>
-                        <textarea name="user_message" placeholder="Message"/>
-                    </div>
-
-                    <div>
-                        <input type="submit" value="ENVOYER" />
+                    <div className="container-btn">
+                        <input className="btn-form" type="submit" value="ENVOYER" />
                     </div>
                 </form>
+            </div>
+
             </div>
         </div>
     )
